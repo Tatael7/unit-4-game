3d4576e2c9fa3b272cd7bca9a41c60c9e2f493ef
T4T4el7 <ochoaarmando13@gmail.com>
2018-12-13 22:24:57 -0800

    successfully created the buttons, made the random number generators for the target number and the buttons as well as being able to have my bnumber variable become the addition of my button choices. i will now work to project my numbers to the html variables.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,83 @@
 var tWins = 0;
 var tLosses = 0;
-var yNum ;
+var yNum = 0 ;
 var tNum ;
 var points ; 
+var ballOne;
+var ballTwo;
+var ballThree;
+var ballFour;
+
+function reset() {
+    points = 0;
+    tNum = Math.floor(Math.random() * 120) + 19;
+    console.log(tNum);
+    yNum = 0;
+    console.log(yNum);
+    ballOne = Math.floor(Math.random() * 12) + 1;  
+    console.log(ballOne);
+    ballTwo = Math.floor(Math.random() * 12) + 1; 
+    console.log(ballTwo) 
+    ballThree = Math.floor(Math.random() * 12) + 1; 
+    console.log(ballThree); 
+    ballFour = Math.floor(Math.random() * 12) + 1;
+    console.log(ballFour);  
+
+};
+
+function play() {
+    $("#ball1").on("click", function() {
+        console.log(ballOne + " is the amount of points you added");
+        yNum = yNum + ballOne;
+        console.log(yNum);
+        console.log(yNum + " vs " + tNum);
+        scoreCheck();
+    });
+    $("#ball2").on("click", function() {
+        console.log(ballTwo + " is the amount of points you added");
+        yNum = yNum + ballTwo;
+        console.log(yNum);
+        console.log(yNum + " vs " + tNum);
+        scoreCheck();
+    });
+    $("#ball3").on("click", function() {
+        console.log(ballThree + " is the amount of points you added");
+        yNum = yNum + ballThree;
+        console.log(yNum);
+        console.log(yNum + " vs " + tNum);
+        scoreCheck();
+    });
+    $("#ball4").on("click", function() {
+        console.log(ballFour + " is the amount of points you added");
+        yNum = yNum + ballFour;
+        console.log(yNum);
+        console.log(yNum + " vs " + tNum);
+        scoreCheck();
+    });
+
+    
+};
+
+function scoreCheck() {
+    
+    if ( yNum === tNum) {
+       console.log("its a match");
+       tWins++;
+       alert("You Win Homie!");
+       reset();
+
+    } else if ( yNum > tNum ) {
+        console.log("its a nay");
+        tLosses++;
+        alert("You Lose, How Sad.");
+        reset();
+    }
 
 
-prompt("por k");
+};
+
+
+reset();
+play();
+scoreCheck();
+
